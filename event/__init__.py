ac774594d0280b53c8615e1a1a052e294ab3ecc0
10994521 <91101317+10994521@users.noreply.github.com>
2021-09-21 11:02:52 +1000

    user 1

---
+++ event/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+#Database initialise
+db=SQLAlchemy()
+
+def create_app():
+    app=Flask(__name__)
+    
+    boostrap = Bootstrap(app)
+
+
+    app.secret_key='somerandomvalue'
+
+    #DatabaseConfig
+    # app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///travel123.sqlite'
+    # db.init_app(app)
+
+    #ImageUploadPat
+    # UPLOAD_FOLDER = '/static/image'
+    # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    #Blueprints
+    from . import views
+    app.register_blueprint(views.mainbp)
+
+    return app

262723e3b6de8697c942af24db7fffb1cfa1e020
AlexW151 <n10753010@qut.edu.au>
2021-10-05 11:52:07 +1000

    Edited database
    added fk and relationships, changed name. changed db name

--- event/models.py
+++ event/models.py
@@ -2,43 +2,52 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
-#unchanged. Don't think it's necessary
+import event
+
+# unchanged. Don't think it's necessary
+
+
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     emailid = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(225), nullable=False)
 
+    events = db.relationship('Event', backref='user')
     comments = db.relationship('Comment', backref='user')
 
-#edited to be relevant to events. Status column incomplete
+# edited to be relevant to events. Status column incomplete
+
+
 class Event(db.Model):
     __tablename__ = 'events'
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     speaker = db.Column(db.String(80))
     description = db.Column(db.String(200))
     dateTime = db.Column(db.DateTime, default=datetime.now())
     address = db.Column(db.String(200))
     image = db.Column(db.String(400))
-    status = db.Column(db.String) 
+    status = db.Column(db.String)
 
-    # comments = db.relationship('Comment', backref='event')
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='event')
 
     def __repr__(self):
         return "<Name: {}>".format(self.name)
 
-#changed occurences of destination to event 
+# changed occurences of destination to event
+
+
 class Comment(db.Model):
-    __tablename__= 'comments'
-    id = db.Column(db.Integer, primary_key= True)
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
     created_at = db.Column(db.DateTime, default=datetime.now())
-    #foreign keys
-    # user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    # event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    # foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from event import create_app
+#from event import create_app#####
 
 if __name__ == '__main__':
     n_app = create_app()
     n_app.run(debug=True)
+
+
+# Use this ocde to create a db file (when terminal doesn't work)
+
+#from event import db, create_app
+#app = create_app()
+#ctx = app.app_context()
+# ctx.push()
+# db.create_all()
+# quit()

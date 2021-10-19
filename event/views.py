d642dc4d4b8644a1502c753b7ba1cc17df85d772
AlexW151 <n10753010@qut.edu.au>
2021-10-19 10:41:25 +1000

    details.html viewing
    added working links for details.html on home page, fixed the bottom content and image links

--- event/views.py
+++ event/views.py
@@ -1,18 +1,20 @@
 from flask import Blueprint, render_template
+from .models import Event
 
 mainbp = Blueprint('main', __name__)
 
 
 @mainbp.route('/')
 def index():
-    return render_template('index.html')
+    events = Event.query.all()
+    return render_template('index.html', events=events)
 
 
 @mainbp.route('/profile')
 def profile():
     return render_template('profile.html')
 
+
 @mainbp.route('/details')
 def details():
     return render_template('details.html')
-
